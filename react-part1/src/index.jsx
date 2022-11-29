6cbe17848d36492fd9156eaa58d16fdf00dab6dd
Daniel Janda <87991817+LaterFriend@users.noreply.github.com>
2022-11-29 01:10:40 +0100

    1

--- react-part1/src/index.jsx
+++ react-part1/src/index.jsx
@@ -1,19 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import logo from "./react-logo.png";
+import App from "./App";
 
-const site = (
-    <section>
-        <img width="40px" src={logo} />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </section>
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(site);
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
